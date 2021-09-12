99a94b47c621c51f2d615d254a438f70f177ad0d
cse-mkamble <cse.mkamble@gmail.com>
2021-09-12 19:38:38 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(express.json())
 // Run Server
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
-    console.log("Server is running on 🔥 ", PORT);
+    console.log("🚀🚀 Server is running on 🔥 ", PORT);
 });
